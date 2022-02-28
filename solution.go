f8ee25f9d134f60f80ea7c5c6843003424b733bb
amilbek <amilbek.nurbolat@gmail.com>
2022-02-28 15:18:27 +0600

    commit

--- solution.go
+++ solution.go
@@ -1,17 +1,9 @@
-package main
+package solution
 
 import (
-	"flag"
 	"github.com/kyokomi/emoji"
 )
 
-func main() {
-	emoji.Print(GetMessage())
-}
-
 func GetMessage() string {
-	emojiName := flag.String("e", "Hello :world_map:!", "emoji name")
-	flag.Parse()
-
-	return *emojiName
+	return emoji.Sprint("Hello :world_map:!")
 }
